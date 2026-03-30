4e2d765136cf7aea310352ae3e092600f87eec43
Your Name <you@example.com>
2026-03-30 01:53:36 +0000

    new try

--- list.c
+++ list.c
@@ -53,7 +53,7 @@ void * nextList(List * list) {
     if(list->current == NULL || list -> current -> next == NULL){
         
     }
-
+}
 // 3. Programe las funciones void * lastList(List * list) y void * prevList(List * list).
 //   - La primera retorna el dato del último elemento en la lista y actualiza el current al nodo correspondiente.
 //   - La segunda función retorna el dato del nodo anterior a current y actualiza el current para que apunte a ese nodo.
